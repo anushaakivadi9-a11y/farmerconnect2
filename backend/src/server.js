77db044ca4520048f1135edaa341643c342e3a64
AAK 191 <anushaakivadi9@gmail.com>
2026-08-10 19:48:34 +0530

    removing wroker service and adding that to farmconnect-api

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,10 +7,14 @@ const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
+require('./workers/emailWorker');
+require('./workers/imageWorker');
+
 // Setup Socket.io
 socketSetup(server);
 
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`👷 Workers listening for jobs...`);
   console.log(`📱 Frontend URL: ${process.env.CORS_ORIGIN || 'http://localhost:5173'}`);
 });
